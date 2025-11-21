684d455f38c283eb77e70b4a5cd5e6ce2ddb21e8
Арсений Браташин <Ars.Bratashin@gmail.com>
2025-11-21 14:08:40 +0300

    2-vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,7 @@
-<script setup lang="ts">
-import ProfileAvatar from '@/components/ProfileAvatar.vue'
-import CategoryList from '@/components/CategoryList.vue'
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="app">
-    <nav class="nav">
-      <ProfileAvatar />
-      <CategoryList />
-    </nav>
-    <main>Контент</main>
-  </div>
+  <RouterView />
 </template>
 
-<style scoped>
-.app {
-  display: flex;
-  gap: 200px;
-  min-height: calc(100vh - 140px);
-  max-width: 1450px;
-  margin: 140px auto 0 auto;
-}
-
-.nav {
-  min-width: 400px;
-}
-</style>
+<style scoped></style>
